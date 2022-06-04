fefde68659f9267180480d9a9ce52c0652cf7834
Wrash <46035160+wrefinity@users.noreply.github.com>
2022-06-04 02:52:11 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,17 +7,19 @@
  */
 int main(void)
 {
-	int x = '0';
-	while (x <= '9')
+	int x;
+
+	for (x = 0; x <= 9; x++)
 	{
-		putchar(x);
-		if (x != '9')
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		++x;
+		putchar((x % 10) + '0');
+		if (x == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
 	}
+
 	putchar('\n');
+
 	return (0);
 }
