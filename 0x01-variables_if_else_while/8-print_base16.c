f49b4f5fa2e93f5030b4caeede5739cf65e0bc82
Wreford <wrefordmessi@gmail.com>
2022-06-03 18:30:53 -0700

    Task C

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,11 +12,8 @@ int main(void)
 
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
-
 	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
-
 	putchar('\n');
-
 	return (0);
 }
