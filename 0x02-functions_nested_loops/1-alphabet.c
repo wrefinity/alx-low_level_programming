96a6b8d1c9e0e7ca4d153ceeffc5a57f697457d4
Wreford <wrefordmessi@gmail.com>
2022-06-07 13:27:52 -0700

    first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet
+ *
+ * Return: Nothing.
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
