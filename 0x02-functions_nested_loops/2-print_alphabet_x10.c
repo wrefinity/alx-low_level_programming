b903969ca73d42a2f23f6e417f6825e1b84438de
Wrash <46035160+wrefinity@users.noreply.github.com>
2022-06-07 21:44:20 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,14 +10,16 @@
 
 void print_alphabet_x10(void)
 {
-	int time, count;
-	
-	for (time = 1; time <= 10; time++)
-	{
-		for (count = 97; count <= 122; count++)
-		{
-			putchar(count);
-		}
-		putchar('\n');	
-	}
+
+int time, count;
+
+for (time = 1; time <= 10; time++)
+{
+for (count = 97; count <= 122; count++)
+{
+putchar(count);
+}
+putchar('\n');
+}
+
 }
