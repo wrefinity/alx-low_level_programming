68e29e3282bb511f956be434fe00c1642a11bc44
Wreford <wrefordmessi@gmail.com>
2022-06-07 13:34:45 -0700

    first commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - This program prints alphabets 10 times
+ *
+ * Description: To print alphabets
+ * Return: Always void
+ */
+
+void print_alphabet_x10(void)
+{
+	int time, count;
+	for (time = 1; time <= 10; time++)
+	{
+		for (count = 97; count <= 122; count++)
+		{
+			putchar(count);
+		}
+		putchar('\n');
+	}
+}
