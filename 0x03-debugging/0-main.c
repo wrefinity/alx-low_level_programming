f98ffba553ea44b02d2215d8b186339c6b7351c6
Wrash <46035160+wrefinity@users.noreply.github.com>
2022-06-08 23:43:53 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main -tests function that prints if integer is positive or negative 
- * Return 0 on sucess
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
 {
-	int z;
+  int i;
 
-	z = 0;
-	positive_or_negative(z);
+  i = 0;
+  positive_or_negative(i);
 
-	return (0);
+  return (0);
 }
