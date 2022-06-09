2dcae11e4ba2c19162698847e8191f80c4b51a4c
Wreford <wrefordmessi@gmail.com>
2022-06-09 09:31:28 -0700

    0-9 count

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print number between 0 and 9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int c = 0;
+
+	for (c = 0; c <= 9; c++)
+		_putchar(c)
+	_putchar('\n')
+}
