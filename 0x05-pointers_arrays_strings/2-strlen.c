9ccb4d8afbebe0d736c8aaaf3ee0a1af8abf7d14
Wreford <wrefordmessi@gmail.com>
2022-06-13 12:10:51 -0700

    Task 2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - checks the length of a string
+ * @s: string to check
+ *
+ * Return: int length of the string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
