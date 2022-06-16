5aea66caa29615d0862e7606f78b25b3aae08fb3
Wreford <wrefordmessi@gmail.com>
2022-06-16 11:33:43 -0700

    Noise is a buffer, more effective than cubicles or booth walls

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,8 +1,10 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdio.h>
 
 /**
- * print_buffer - Prints a buffer 10 bytes at a time
+ * print_buffer - Prints a buffer 10 bytes at a time, starting with
+ *                the byte position, then showing the hex content,
+ *                then displaying printable charcaters.
  * @b: The buffer to be printed.
  * @size: The number of bytes to be printed from the buffer.
  */
