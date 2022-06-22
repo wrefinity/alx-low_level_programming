4cb6bb386fbf489d9377acf4ec65d2dbce98ba0d
Wrash <46035160+wrefinity@users.noreply.github.com>
2022-06-22 18:22:09 +0100

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,9 +8,9 @@ i#include "main.h"
 
 void _print_rev_recursion(char *s)
 {
-	if (*(s + 1)  == '\0')
-		return;
-
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
