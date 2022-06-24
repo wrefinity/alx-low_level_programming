548d15a5a89811b29bb9f4bc4d30cd5a38f848df
Wrash <46035160+wrefinity@users.noreply.github.com>
2022-06-24 22:31:00 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,30 +4,30 @@
 
 /**
 * main - function to adds numbers
-* @argc: number of command line arguments passed
+* @argc: commandline argument passed to the function
 * @argv: argument vector of pointers to strings
 *
 * Return: 0 if no errors, else 1
 */
 int main(int argc, char *argv[])
 {
-	int sum = 0, i, j;
+int sum = 0, i, j;
 
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j]; j++)
-		{
-			if (isdigit(argv[i][j]) == 0)
-			{
-				puts("Error\n");
-				return (1);
-			}
-		}
-	}
-	for (i = 1; i < argc; i++)
-	{
-		sum += atoi(argv[i]);
-	}
-	printf("%d\n", sum);
-	return (0);
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j]; j++)
+{
+if (isdigit(argv[i][j]) == 0)
+{
+puts("Error");
+return (1);
+}
+}
+}
+for (i = 1; i < argc; i++)
+{
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
+return (0);
 }
