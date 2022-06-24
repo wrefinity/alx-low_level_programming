22eec302bb03e7e711da5b367b434e3ea6b37309
Wreford <wrefordmessi@gmail.com>
2022-06-24 13:28:39 -0700

    function to print function namee

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - fucntion tp Print its own name
+ * @argc: define the command line arguments
+ * @argv: defines the array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Alx Task on 0x0A. C - argc, argv
