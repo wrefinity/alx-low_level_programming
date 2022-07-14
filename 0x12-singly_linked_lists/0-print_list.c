a101054afa0e2d0d6ebe8df2718f89f89a3a8880
Wreford <wrefordmessi@gmail.com>
2022-07-14 08:23:02 -0700

    update for task 1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,12 +8,19 @@
   */
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t i = 0;
 
-	for (i = 0; h; i++)
+	while (h)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		i++;
 		h = h->next;
 	}
+
 	return (i);
 }
