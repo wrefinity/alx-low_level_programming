3318e65f47c3f2f703668c760876ea921b59e54b
Wrash <46035160+wrefinity@users.noreply.github.com>
2022-07-14 16:45:17 +0100

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,39 +1,5 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <stddef.h>
-/**
-  * _strlen - gets String length
-  * @s: string
-  * Return: length of the string
-  */
-int _strlen(const char *s)
-{
-	int x;
-
-	for (x = 0; s[x]; x++)
-		;
-	return (x);
-}
-/**
-  * _strdup - create string duplicate
-  * @src: source string
-  * Return: dest string
-  */
-void *_strdup(const char *src)
-{
-	int l, i;
-	char *dest;
-
-	l = _strlen(src);
-	dest = malloc((l + 1) * sizeof(char));
-	if (dest == NULL)
-		return (NULL);
-	for (i = 0; src[i]; i++)
-		dest[i] = src[i];
-	dest[i] = '\0';
-	return (dest);
-}
+#include <string.h>
 /**
   * add_node_end - add new nodes to the end of the list
   * @head: current place in the list
@@ -42,30 +8,39 @@ void *_strdup(const char *src)
   */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new, *current;
-	char *dup_str;
+	char *dup;
+	int len;
+	list_t *new, *last;
 
-	if (str == NULL)
-		return (NULL);
-	dup_str = _strdup(str);
-	if (dup_str == NULL)
-		return (NULL);
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-	new->str = dup_str;
-	new->len = _strlen(str);
+
+	dup = strdup(str);
+	if (str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = dup;
+	new->len = len;
 	new->next = NULL;
 
 	if (*head == NULL)
-	{
 		*head = new;
-		return (*head);
+
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new;
 	}
-	current = *head;
-	while (current->next != NULL)
-		current = current->next;
-	current->next = new;
+
 	return (*head);
 }
 
