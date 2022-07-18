42612a7dcd7aa2d82da30678e92373748c862671
Wrash <46035160+wrefinity@users.noreply.github.com>
2022-07-18 20:57:11 +0100

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,10 +2,11 @@
 #define LISTS_H
 
 /*
+ * File: lists.h
+ * Auth: Andrew Ishaku Wreford
  * Desc: Header file containing prototypes and definitions for all functions
  *       and types written in the 0x12-more_singly_linked_lists directory.
  */
-
 
 /**
  * struct listint_s - singly linked list
