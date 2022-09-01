304e82d02e405a5f3114f1683e49909cae17863f
Andrew Eshak Wreford <46035160+wrefinity@users.noreply.github.com>
2022-09-01 13:19:53 +0100

    a function that adds a new node at the beginning of a dlistint_t list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,20 +10,29 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node;
+	dlistint_t *new;
+	dlistint_t *h;
 
-	new_node = malloc(sizeof(dlistint_t));
-
-	if (new_node == NULL)
-	{
-		free(new_node);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	h = *head;
+
+	if (h != NULL)
+	{
+		while (h->prev != NULL)
+			h = h->prev;
 	}
 
-	new_node->n = n;
-	new_node->next = *head;
-	new_node->prev = NULL;
-	*head = new_node;
+	new->next = h;
 
-	return (new_node);
+	if (h != NULL)
+		h->prev = new;
+
+	*head = new;
+
+	return (new);
 }
