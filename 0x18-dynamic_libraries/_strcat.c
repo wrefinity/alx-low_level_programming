d83d2e83a809eaf78a70785a29c22e45168b040d
Andrew Eshak Wreford <46035160+wrefinity@users.noreply.github.com>
2022-09-05 20:07:59 +0100

    Update _strcat.c

--- 0x18-dynamic_libraries/_strcat.c
+++ 0x18-dynamic_libraries/_strcat.c
@@ -24,3 +24,34 @@ char *_strcat(char *dest, char *src)
 	dest[length] = '\0';
 	return (dest);
 }
+
+
+/**
+ * _strncat - function that concatenates two strings. it will use
+ * at most n bytes from src doesnt not need to be null-terminated
+ * @dest: that point to the destination str
+ * @src: that point tothe soure str
+ * @n: the number that will be concatenated
+ *
+ * Return: pointer to the resulting string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int length, i;
+
+	/* length is the destination of string */
+	/* i count the bytes from the source */
+
+	length = 0;
+
+	while (dest[length] != '\0')
+	{
+		length++;
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++, length++)
+	{
+		dest[length] = src[i];
+	}
+	dest[length] = '\0';
+	return (dest);
+}
