c891dbe1df02dc3c9d8c2b9817f08092547f41df
Wreford <wrefordmessi@gmail.com>
2022-09-23 13:13:28 -0700

    betty fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,23 +10,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long position, size;
 	hash_node_t *node;
-	
+
 	if (ht  == NULL || value == NULL || key == NULL)
 		return (0);
-	
 	size = ht->size;
 	position = key_index((const unsigned char *)key, size);
-	
 	if (ht->array[position] != NULL && strcmp(ht->array[position]->key, key) == 0)
 	{
 		ht->array[position]->value = strdup(value);
 		return (1);
 	}
-	
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
-	
 	node->key = strdup(key);
 	node->value = strdup(value);
 	node->next = ht->array[position];
