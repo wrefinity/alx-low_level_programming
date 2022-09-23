b8e63837e96e891dc2528ad5142ba4ceb40ab830
Andrew Eshak Wreford <46035160+wrefinity@users.noreply.github.com>
2022-09-23 21:08:41 +0100

    fix error on node creation

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -30,6 +30,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->key = strdup(key);
 	node->value = strdup(value);
 	node->next = ht->array[position];
-	ht->array[position] = new_node;
+	ht->array[position] = node;
 	return (1);
 }
