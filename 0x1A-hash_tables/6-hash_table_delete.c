0e9356ab2c5868775a722183e1e21c1ae00f88ff
Andrew Eshak Wreford <46035160+wrefinity@users.noreply.github.com>
2022-09-23 21:45:25 +0100

    a function that deletes a hash table.

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,28 +1,25 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - Deletes a hash table.
- * @ht: A pointer to a hash table.
+ * hash_table_delete - deletes a hash table.
+ * @ht: hash table pointer
  */
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_table_t *head = ht;
 	hash_node_t *node, *tmp;
-	unsigned long int i;
+	unsigned long int x;
 
-	for (i = 0; i < ht->size; i++)
+	for (x = 0; x < ht->size && ht->array[x] != NULL; x++)
 	{
-		if (ht->array[i] != NULL)
+		node = ht->array[x];
+		while (node != NULL)
 		{
-			node = ht->array[i];
-			while (node != NULL)
-			{
-				tmp = node->next;
-				free(node->key);
-				free(node->value);
-				free(node);
-				node = tmp;
-			}
+			tmp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = tmp;
 		}
 	}
 	free(head->array);
